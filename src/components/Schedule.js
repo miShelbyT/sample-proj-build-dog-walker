c4bbd21b6af952bbc25f248e067e63ccea391a23
Shelby Talbert <shelby.talbert@flatironschool.com>
2023-01-14 13:59:00 -0500

    updating my form

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -3,16 +3,7 @@ import React from 'react'
 function Schedule() {
   return (
     <div><form>
-      <input type="text" />
-      <input type="date" />
-      <input type="text" />
-      <input type="date" />
-      <input type="text" />
-      <input type="date" />
-      <input type="text" />
-      <input type="date" />
-      <input type="text" />
-      <input type="date" />
+      <input />
       </form></div>
   )
 }
